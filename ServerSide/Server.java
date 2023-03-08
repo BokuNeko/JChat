5022c1e92a56e23c793457f52af4a47f538880f3
BokuNeko <kat.eduward@gmail.com>
2023-03-08 11:41:12 +0100

    Aggiunta ciclo principale di ascolto connessioni al server

--- ServerSide/Server.java
+++ ServerSide/Server.java
@@ -1,7 +1,15 @@
+import java.net.*;
+
 public class Server
 {
     public static void main(String args[])
     {
-
-    }
-}
+        Socket socket;
+        ServerSocket serverSocket = new ServerSocket(6000);
+        //Loop infinito per ascolto connessioni
+        while(true)
+        {
+            socket = serverSocket.accept();
+        }//end while
+    }//end main
+}//end class
