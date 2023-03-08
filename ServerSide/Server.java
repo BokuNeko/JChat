d636f2eb4a4551ce5e0b0432a17e4bb2b23105ff
BokuNeko <kat.eduward@gmail.com>
2023-03-08 11:44:55 +0100

    Riscrittura ciclo, ora il progetto e l'IDE sono funzionanti.

--- ServerSide/Server.java
+++ ServerSide/Server.java
@@ -1,3 +1,6 @@
+package ServerSide;
+
+import java.io.IOException;
 import java.net.*;
 
 public class Server
@@ -5,11 +8,19 @@ public class Server
     public static void main(String args[])
     {
         Socket socket;
-        ServerSocket serverSocket = new ServerSocket(6000);
-        //Loop infinito per ascolto connessioni
-        while(true)
+        try
         {
-            socket = serverSocket.accept();
-        }//end while
+            ServerSocket serverSocket = new ServerSocket(6000);
+            //Loop infinito per ascolto connessioni
+            while(true)
+            {
+                socket = serverSocket.accept();
+            }//end while
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }//end main
 }//end class
