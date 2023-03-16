647f60add6b48d184a38aa8abf0dfe1c09ab202b
BokuNeko <kat.eduward@gmail.com>
2023-03-16 17:50:47 +0100

    Update Client.java
    Correzione errori classe Client (gestione errore IOE)

--- ClientSide/Client.java
+++ ClientSide/Client.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -7,14 +8,21 @@ public class Client
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ciao questo è il programma scritto da Edoardo per utilizzare una chat");
-        System.out.println("Vuoi fare una prova di connessione socket?")
+        System.out.println("Vuoi fare una prova di connessione socket?");
 
         String sceltaUtente = scanner.nextLine();
         if(sceltaUtente.equals("y") )
         {
             System.out.println("Hai scelto si");
             
-            Socket socket = new Socket("localHost", 6000);
+            try
+            {
+                Socket socket = new Socket("localHost", 6000);
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
         }//end if
         else if(sceltaUtente.equals("n") )
         {
